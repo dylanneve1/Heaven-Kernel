d5d1875348a01ffc6e259e7006e766047d693f06
dylanneve1 <dylanneve1@gmail.com>
2017-12-05 21:32:55 +0000

    Hmmm

---
+++ include/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#1 SMP PREEMPT Tue Dec 5 21:24:02 UTC 2017"
+#define LINUX_COMPILE_BY "dylanneve1"
+#define LINUX_COMPILE_HOST "instance-1"
+#define LINUX_COMPILER "gcc version 7.2.1 20171016 (crosstool-NG crosstool-ng-1.23.0-236-g42429d9 - GCC 7.2.1 / Linux 4.13.7 by djb77) "
